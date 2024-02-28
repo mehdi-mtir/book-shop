66b7945ec672764518bee260c60c39046ea99a2c
MEHDI MTIR <mehdi.mtir@gmail.com>
2024-02-28 09:45:44 +0100

    Mise en place des routes après création des modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListBooksComponent } from './books/list-books/list-books.component';
-import { AddBookComponent } from './books/add-book/add-book.component';
-import { EditBookComponent } from './books/edit-book/edit-book.component';
 
 const routes: Routes = [
-  {path : "books", component : ListBooksComponent, pathMatch : "full"},
-  {path : "books/add", component : AddBookComponent},
-  {path : "books/edit", component : EditBookComponent},
-  {path : "", redirectTo : "books", pathMatch : "full"}
+  {
+    path: 'books',
+    loadChildren: () => import('./books/books.module').then(m => m.BooksModule)
+  },
+  {
+    path: 'authors',
+    loadChildren: () => import('./authors/authors.module').then(m => m.AuthorsModule)
+  }
 ];
 
 @NgModule({
--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBooksComponent } from './list-books/list-books.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component : ListBooksComponent, pathMatch : "full"},
+  {path : "add", component : AddBookComponent},
+  {path : "edit", component : EditBookComponent},
+  {path : "", redirectTo : "books", pathMatch : "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
